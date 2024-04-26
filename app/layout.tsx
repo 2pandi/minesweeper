c3645cb8407a0b551443793170d1001f50f7e969
2pandi <coder.2pandi@gmail.com>
2024-04-26 10:07:07 +0900

    style 폴더

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/style/global.scss";
+import "@/style/components.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
