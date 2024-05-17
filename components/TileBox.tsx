6d79eb8949958a3df4c2c0822458a45e0c901722
2pandi <coder.2pandi@gmail.com>
2024-05-17 12:34:02 +0900

    mode별 starting logic 수정

--- components/TileBox.tsx
+++ components/TileBox.tsx
@@ -3,13 +3,14 @@
 import React from "react";
 import Tile from "./Tile";
 import { useGameStore } from "@/zustand/gameStore";
-import { makeMap } from "@/util";
+import { bangTile, makeMap } from "@/util";
 
 export default function TileBox() {
   const {
     mapXLen,
     mapYLen,
     start,
+    mode,
     totalBomb,
     status,
     map,
@@ -26,93 +27,8 @@ export default function TileBox() {
     const newOpenTileMap = openTileMap.map((line) => line.map((v) => v));
     newOpenTileMap[y][x] = "O";
 
-    const bang = (x: number, y: number) => {
-      let newY1 = y - 1;
-      while (newY1 >= 0 && newOpenTileMap[newY1][x] !== "O") {
-        newOpenTileMap[newY1][x] = "O";
-        if (!map[newY1][x]) bang(x, newY1);
-        if (map[newY1][x]) break;
-        newY1--;
-      }
-
-      let newY2 = y + 1;
-      while (newY2 < map.length && newOpenTileMap[newY2][x] !== "O") {
-        newOpenTileMap[newY2][x] = "O";
-        if (!map[newY2][x]) bang(x, newY2);
-        if (map[newY2][x]) break;
-        newY2++;
-      }
-
-      let newX1 = x - 1;
-      while (newX1 >= 0 && newOpenTileMap[y][newX1] !== "O") {
-        newOpenTileMap[y][newX1] = "O";
-        if (!map[y][newX1]) bang(newX1, y);
-        if (map[y][newX1]) break;
-        newX1--;
-      }
-
-      let newX2 = x + 1;
-      while (newX2 < map[0].length && newOpenTileMap[y][newX2] !== "O") {
-        newOpenTileMap[y][newX2] = "O";
-        if (!map[y][newX2]) bang(newX2, y);
-        if (map[y][newX2]) break;
-        newX2++;
-      }
-
-      let newY3 = y - 1,
-        newX3 = x - 1;
-      while (newY3 >= 0 && newX3 >= 0 && newOpenTileMap[newY3][newX3] !== "O") {
-        newOpenTileMap[newY3][newX3] = "O";
-        if (!map[newY3][newX3]) bang(newX3, newY3);
-        if (map[newY3][newX3]) break;
-        newY3--;
-        newX3--;
-      }
-
-      let newY4 = y + 1,
-        newX4 = x - 1;
-      while (
-        newY4 < map.length &&
-        newX4 >= 0 &&
-        newOpenTileMap[newY4][newX4] !== "O"
-      ) {
-        newOpenTileMap[newY4][newX4] = "O";
-        if (!map[newY4][newX4]) bang(newX4, newY4);
-        if (map[newY4][newX4]) break;
-        newY4++;
-        newX4--;
-      }
-
-      let newY5 = y + 1,
-        newX5 = x + 1;
-      while (
-        newY5 < map.length &&
-        newX5 < map[0].length &&
-        newOpenTileMap[newY5][newX5] !== "O"
-      ) {
-        newOpenTileMap[newY5][newX5] = "O";
-        if (!map[newY5][newX5]) bang(newX5, newY5);
-        if (map[newY5][newX5]) break;
-        newY5++;
-        newX5++;
-      }
-
-      let newY6 = y - 1,
-        newX6 = x + 1;
-      while (
-        newY6 >= 0 &&
-        newX6 < map[0].length &&
-        newOpenTileMap[newY6][newX6] !== "O"
-      ) {
-        newOpenTileMap[newY6][newX6] = "O";
-        if (!map[newY6][newX6]) bang(newX6, newY6);
-        if (map[newY6][newX6]) break;
-        newY6--;
-        newX6++;
-      }
-    };
     if (isMapSet && !map[y][x]) {
-      bang(x, y);
+      bangTile(x, y, newOpenTileMap, map);
     }
 
     setOpenTileMap(newOpenTileMap);
@@ -124,12 +40,21 @@ export default function TileBox() {
       setStartingPoint([x, y]);
     }
 
-    openTile(x, y);
+    if (mode === "B") {
+      openTile(x, y);
+    }
   };
 
   React.useEffect(() => {
     if (status === "P" && startingPoint[0] >= 0) {
-      const newMap = makeMap(map, startingPoint, mapXLen, mapYLen, totalBomb);
+      const newMap = makeMap(
+        map,
+        startingPoint,
+        mapXLen,
+        mapYLen,
+        totalBomb,
+        mode
+      );
       setMap(newMap);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -143,7 +68,7 @@ export default function TileBox() {
   }, [map, startingPoint]);
 
   React.useEffect(() => {
-    if (isMapSet) {
+    if (isMapSet && mode === "B") {
       openTile(startingPoint[0], startingPoint[1]);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
--- util/index.ts
+++ util/index.ts
@@ -1,12 +1,13 @@
-import { BOMB } from "@/constants";
-import { T_mapTile } from "@/interface";
+import { BOMB, GAME_MODE } from "@/constants";
+import { T_mapTile, T_openMapTile } from "@/interface";
 
 const placeBomb = (
   emptyMap: Array<Array<T_mapTile>>,
   totalBomb: number,
   x: number,
   y: number,
-  startingPoint: [number, number]
+  startingPoint: [number, number],
+  mode: keyof typeof GAME_MODE
 ) => {
   const [startX, startY] = startingPoint;
   let leftBomb = totalBomb;
@@ -15,14 +16,16 @@ const placeBomb = (
     const xPosition = Math.floor(Math.random() * x);
     const yPosition = Math.floor(Math.random() * y);
 
-    if (
-      emptyMap[yPosition][xPosition] ||
-      (startX - 2 < xPosition &&
-        xPosition < startX + 2 &&
-        startY - 2 < yPosition &&
-        yPosition < startY + 2)
-    )
-      continue;
+    if (mode === "B") {
+      if (
+        emptyMap[yPosition][xPosition] ||
+        (startX - 2 < xPosition &&
+          xPosition < startX + 2 &&
+          startY - 2 < yPosition &&
+          yPosition < startY + 2)
+      )
+        continue;
+    }
 
     emptyMap[yPosition][xPosition] = BOMB;
     leftBomb--;
@@ -98,11 +101,101 @@ export const makeMap = (
   startingPoint: [number, number],
   x: number,
   y: number,
-  totalBomb: number
+  totalBomb: number,
+  mode: keyof typeof GAME_MODE
 ) => {
-  const newMap = placeBomb(map, totalBomb, x, y, startingPoint);
+  const newMap = placeBomb(map, totalBomb, x, y, startingPoint, mode);
 
   return placeNumber(newMap);
 };
 
-export const openTile = (x: number, y: number) => {};
+export const bangTile = (
+  x: number,
+  y: number,
+  openTileMap: T_openMapTile[][],
+  map: T_mapTile[][]
+) => {
+  let newY1 = y - 1;
+  while (newY1 >= 0 && openTileMap[newY1][x] !== "O") {
+    openTileMap[newY1][x] = "O";
+    if (!map[newY1][x]) bangTile(x, newY1, openTileMap, map);
+    if (map[newY1][x]) break;
+    newY1--;
+  }
+
+  let newY2 = y + 1;
+  while (newY2 < map.length && openTileMap[newY2][x] !== "O") {
+    openTileMap[newY2][x] = "O";
+    if (!map[newY2][x]) bangTile(x, newY2, openTileMap, map);
+    if (map[newY2][x]) break;
+    newY2++;
+  }
+
+  let newX1 = x - 1;
+  while (newX1 >= 0 && openTileMap[y][newX1] !== "O") {
+    openTileMap[y][newX1] = "O";
+    if (!map[y][newX1]) bangTile(newX1, y, openTileMap, map);
+    if (map[y][newX1]) break;
+    newX1--;
+  }
+
+  let newX2 = x + 1;
+  while (newX2 < map[0].length && openTileMap[y][newX2] !== "O") {
+    openTileMap[y][newX2] = "O";
+    if (!map[y][newX2]) bangTile(newX2, y, openTileMap, map);
+    if (map[y][newX2]) break;
+    newX2++;
+  }
+
+  let newY3 = y - 1,
+    newX3 = x - 1;
+  while (newY3 >= 0 && newX3 >= 0 && openTileMap[newY3][newX3] !== "O") {
+    openTileMap[newY3][newX3] = "O";
+    if (!map[newY3][newX3]) bangTile(newX3, newY3, openTileMap, map);
+    if (map[newY3][newX3]) break;
+    newY3--;
+    newX3--;
+  }
+
+  let newY4 = y + 1,
+    newX4 = x - 1;
+  while (
+    newY4 < map.length &&
+    newX4 >= 0 &&
+    openTileMap[newY4][newX4] !== "O"
+  ) {
+    openTileMap[newY4][newX4] = "O";
+    if (!map[newY4][newX4]) bangTile(newX4, newY4, openTileMap, map);
+    if (map[newY4][newX4]) break;
+    newY4++;
+    newX4--;
+  }
+
+  let newY5 = y + 1,
+    newX5 = x + 1;
+  while (
+    newY5 < map.length &&
+    newX5 < map[0].length &&
+    openTileMap[newY5][newX5] !== "O"
+  ) {
+    openTileMap[newY5][newX5] = "O";
+    if (!map[newY5][newX5]) bangTile(newX5, newY5, openTileMap, map);
+    if (map[newY5][newX5]) break;
+    newY5++;
+    newX5++;
+  }
+
+  let newY6 = y - 1,
+    newX6 = x + 1;
+  while (
+    newY6 >= 0 &&
+    newX6 < map[0].length &&
+    openTileMap[newY6][newX6] !== "O"
+  ) {
+    openTileMap[newY6][newX6] = "O";
+    if (!map[newY6][newX6]) bangTile(newX6, newY6, openTileMap, map);
+    if (map[newY6][newX6]) break;
+    newY6--;
+    newX6++;
+  }
+};
