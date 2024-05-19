85a83ea0a18fefbae1edccdd44d379691fac7300
2pandi <coder.2pandi@gmail.com>
2024-05-19 11:47:31 +0900

    리팩토링

--- components/TileBox.tsx
+++ components/TileBox.tsx
@@ -117,28 +117,31 @@ export default function TileBox() {
 
   const tempTileClassSetter = (x: number, y: number) => {
     const totalFlag = countFlagAroundTile(x, y, openTileMap);
+
     if (totalFlag < (map[y][x] as number)) {
       const newTempClassTiles: [number, number][] = [];
-      if (y - 1 >= 0) {
-        if (openTileMap[y - 1][x] !== "F") newTempClassTiles.push([x, y - 1]);
 
-        if (x - 1 >= 0 && openTileMap[y - 1][x - 1] !== "F")
-          newTempClassTiles.push([x - 1, y - 1]);
-        if (x + 1 < openTileMap[0].length && openTileMap[y - 1][x + 1] !== "F")
-          newTempClassTiles.push([x + 1, y - 1]);
-      }
-      if (y + 1 < openTileMap.length) {
-        if (openTileMap[y + 1][x] !== "F") newTempClassTiles.push([x, y + 1]);
+      const directions = [
+        { dx: 0, dy: -1 }, // 위
+        { dx: 0, dy: 1 }, // 아래
+        { dx: -1, dy: 0 }, // 왼쪽
+        { dx: 1, dy: 0 }, // 오른쪽
+        { dx: -1, dy: -1 }, // 왼쪽 위
+        { dx: -1, dy: 1 }, // 왼쪽 아래
+        { dx: 1, dy: -1 }, // 오른쪽 위
+        { dx: 1, dy: 1 }, // 오른쪽 아래
+      ];
 
-        if (x - 1 >= 0 && openTileMap[y + 1][x - 1] !== "F")
-          newTempClassTiles.push([x - 1, y + 1]);
-        if (x + 1 < openTileMap[0].length && openTileMap[y + 1][x + 1] !== "F")
-          newTempClassTiles.push([x + 1, y + 1]);
-      }
-      if (x - 1 >= 0 && openTileMap[y][x - 1] !== "F")
-        newTempClassTiles.push([x - 1, y]);
-      if (x + 1 < openTileMap[0].length && openTileMap[y][x + 1] !== "F")
-        newTempClassTiles.push([x + 1, y]);
+      const isValidPosition = (x: number, y: number) =>
+        x >= 0 && y >= 0 && x < openTileMap[0].length && y < openTileMap.length;
+
+      directions.forEach(({ dx, dy }) => {
+        const newX = x + dx;
+        const newY = y + dy;
+
+        if (isValidPosition(newX, newY) && openTileMap[newY][newX] !== "F")
+          newTempClassTiles.push([newX, newY]);
+      });
 
       setTempClassTiles(newTempClassTiles);
     }
@@ -163,6 +166,7 @@ export default function TileBox() {
             start();
             setStartingPoint([x, y]);
           }
+          openTile(x, y);
 
           break;
         default:
