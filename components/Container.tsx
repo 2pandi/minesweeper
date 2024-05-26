41b3fc754cd0d627243b252eacdb2fc10d0fc240
2pandi <coder.2pandi@gmail.com>
2024-05-26 14:38:11 +0900

    mobile browser resizing시 touch event 비활성화

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import Modal from "./Modal";
 import useWin from "@/hooks/useWin";
@@ -5,11 +7,46 @@ import useWin from "@/hooks/useWin";
 interface I_containerProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export default function Container(props: I_containerProps) {
+  const ref = React.useRef<HTMLDivElement>(null);
+
+  const disableTouch = () => {
+    if (ref.current) {
+      ref.current.classList.add("no-touch");
+    }
+  };
+
+  const enableTouch = () => {
+    if (ref.current) {
+      ref.current.classList.remove("no-touch");
+    }
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("resize", disableTouch);
+    window.addEventListener("orientationchange", disableTouch);
+
+    const reEnableTouch = () => {
+      setTimeout(enableTouch, 500);
+    };
+
+    window.addEventListener("resize", reEnableTouch);
+    window.addEventListener("orientationchange", reEnableTouch);
+
+    return () => {
+      window.removeEventListener("resize", disableTouch);
+      window.removeEventListener("orientationchange", disableTouch);
+      window.removeEventListener("resize", reEnableTouch);
+      window.removeEventListener("orientationchange", reEnableTouch);
+    };
+  }, []);
+
   useWin();
 
   return (
     <React.Fragment>
-      <div className="container">{props.children}</div>
+      <div ref={ref} className="container">
+        {props.children}
+      </div>
       <Modal />
     </React.Fragment>
   );
