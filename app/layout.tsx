bbbdf02dbc769a7a49869d7483e9a7af26f13767
2pandi <coder.2pandi@gmail.com>
2024-05-26 13:39:16 +0900

    viewport 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,9 @@ export default function RootLayout({
     <html lang="ko">
       <meta
         name="viewport"
-        content="width=device-width, initial-scale=1.0"
-      ></meta>
+        content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"
+      />
+
       <body>{children}</body>
     </html>
   );
