d68ba1caf888a59bea00d68de27a85ccb2ed5ded
2pandi <coder.2pandi@gmail.com>
2025-07-02 11:34:08 +0900

    adsense추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-import type { Metadata } from "next";
-import "@/style/global.scss";
-import "@/style/components.scss";
+import type { Metadata } from 'next';
+import '@/style/global.scss';
+import '@/style/components.scss';
 
 export const metadata: Metadata = {
-  title: "Minesweeper",
+  title: 'Minesweeper',
   description: "2pandi's toy-project",
 };
 
@@ -18,7 +18,11 @@ export default function RootLayout({
         name="viewport"
         content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"
       />
-
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7784987752318795"
+        crossOrigin="anonymous"
+      ></script>
       <body>{children}</body>
     </html>
   );
